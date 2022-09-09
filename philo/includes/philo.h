c0a1db65333bb63a795a7cff6a7f4953e5f60966
byaliego <yogun@student.42heilbronn.de>
2022-09-09 17:32:54 +0200

    minor update

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: yogun <yogun@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/08 21:05:09 by yogun             #+#    #+#             */
-/*   Updated: 2022/09/09 14:05:00 by yogun            ###   ########.fr       */
+/*   Updated: 2022/09/09 16:49:01 by yogun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include <stdio.h>
 # include <unistd.h>
 # include <stdlib.h>
+# include <pthread.h>
 
 void wrong_input();
 
--- philo/src/philo.c
+++ philo/src/philo.c
@@ -6,18 +6,41 @@
 /*   By: yogun <yogun@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/08 21:04:51 by yogun             #+#    #+#             */
-/*   Updated: 2022/09/09 14:04:11 by yogun            ###   ########.fr       */
+/*   Updated: 2022/09/09 14:22:54 by yogun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
+int	ft_check(int argc, char **argv)
+{
+	if (ft_atoi(argv[1]) < 0 || ft_atoi(argv[2]) < 0 || ft_atoi(argv[3]) < 0 || ft_atoi(argv[4]) < 0)
+	{
+		printf("Wrong input parameters!\n");
+		return (0);
+	}
+	if (argc == 6 && ft_atoi(argv[5]) < 1)
+		return (0);
+	if (ft_atoi(argv[1]) > 0 && ft_atoi(argv[2]) == 0)
+	{
+		printf("0ms 1 died!\n");
+		return (0);
+	}
+	if (ft_atoi(argv[1]) == 1)
+	{
+		printf("0ms 1 has taken a fork\n");
+		printf("%ims 1 died!\n", ft_atoi(argv[2]));
+		return (0);
+	}
+	return (ft_atoi(argv[1]));
+}
 
 
 int	main(int argc, char **argv)
 {
 	if (argc == 5 || argc == 6)
 	{
+		
 	}
 	else
 		wrong_input();
