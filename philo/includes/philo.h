10671c485ba5b382969a614e8cda6d0bdb4f916a
byaliego <yogun@student.42heilbronn.de>
2022-09-09 14:06:02 +0200

    head file has been updated

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,7 +6,17 @@
 /*   By: yogun <yogun@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/08 21:05:09 by yogun             #+#    #+#             */
-/*   Updated: 2022/09/08 21:05:10 by yogun            ###   ########.fr       */
+/*   Updated: 2022/09/09 14:05:00 by yogun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+void wrong_input();
+
+#endif
